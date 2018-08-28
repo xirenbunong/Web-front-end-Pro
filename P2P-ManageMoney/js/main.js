00c388fa95239622f931c8c8ffbaedf958e46734
xirenbunong <m18791744718@163.com>
2018-08-28 18:27:59 +0800

    P2P-ManageMoney 响应式理财网站 预览网站http://tinyurl.com/yaawa6kj

---
+++ P2P-ManageMoney/js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+      items:            1,
+      loop:             true,
+      // itemsDesktop :    [1199,3],
+      // itemsDesktopSmall : [979,3],
+      // itemsTablet:      [580,3],
+      // itemsMobile:      [479,3],
+      autoplay:         true,
+      autoplayTimeout:  3000,
+      autoplayHoverPause: true
+  });
+});
